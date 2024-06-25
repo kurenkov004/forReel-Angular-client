bc01c7b895b16d486c1e1e2fe6a2bfb1327aeb5d
kurenkov004 <mary.kurenkov@gmail.com>
2024-06-25 18:59:13 -0400

    hooked up registration & login components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forReel-Angular-client';
+
+  constructor(public dialog: MatDialog) {}
+
+  //function that will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    //"dialog" below references the "dialog" that was defined in the constructor on line 13
+    this.dialog.open(UserRegistrationFormComponent, {
+      //assigning the dialog width
+      width: '480px'
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '480px'
+    });
+  }
 }
